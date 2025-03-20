5ab8a0e6f6736e776a00f790ca796a9c6c23d495
DommSite <johan.johansson06@live.se>
2025-03-20 12:40:43 +0100

    oops

---
+++ IAttack.cs
@@ -0,0 +1,7 @@
+namespace monogamecheatsheet
+{
+    public interface IAttack
+    {
+         
+    }
+}
---
+++ tierOneEnemy.cs
@@ -0,0 +1,7 @@
+namespace monogamecheatsheet
+{
+    public class tierOneEnemy
+    {
+        
+    }
+}
